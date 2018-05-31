86a036f9f8790b28b9d411d5a4606ff174f04dc3
Kacpro <kacperj97@gmail.com>
2018-05-31 21:32:36 +0200

    Testing replicator

--- src/main/scala/sample/cluster/simple/SimpleClusterListener.scala
+++ src/main/scala/sample/cluster/simple/SimpleClusterListener.scala
@@ -3,17 +3,21 @@ package sample.cluster.simple
 import akka.cluster.Cluster
 import akka.cluster.ClusterEvent._
 import akka.actor.{Actor, ActorLogging, ActorRef, RootActorPath}
-import akka.cluster.ddata.ORMap
-import akka.cluster.ddata.ORMultiMap
+import akka.cluster.ddata.Replicator._
+import akka.cluster.ddata._
 import akka.cluster.pubsub.DistributedPubSub
 import sample.cluster.transformation.BackendRegistration
+import scala.concurrent.duration._
+import akka.cluster.ddata.ORMultiMap
+
 
 
 class SimpleClusterListener extends Actor with ActorLogging {
 
   val cluster = Cluster(context.system)
+  val replicator = DistributedData(context.system).replicator
   val mediator: ActorRef = DistributedPubSub(context.system).mediator
-  var dataMap = ORMultiMap.empty[String, String]
+  val dataMap = ORSetKey[(String, String)]("key")
 
   // subscribe to cluster changes, re-subscribe when restart
   override def preStart(): Unit = {
@@ -21,8 +25,10 @@ class SimpleClusterListener extends Actor with ActorLogging {
       classOf[MemberEvent], classOf[UnreachableMember])
   }
 
-  override def postStop(): Unit = cluster.unsubscribe(self)
+  replicator ! Subscribe(dataMap, self)
 
+  override def postStop(): Unit = cluster.unsubscribe(self)
+  var kkey = ""
   def receive = {
     case msg : String =>
       log.info("hello darkness my old friend")
@@ -45,15 +51,30 @@ class SimpleClusterListener extends Actor with ActorLogging {
 
     case UnreachableMember(member) =>
 
-      println(dataMap.getEntries())
+ //     println(dataMap.getEntries())
 
       log.info("Member detected as unreachable: {}", member)
     case MemberRemoved(member, previousStatus) =>
       log.info("Member is Removed: {} after {}",
         member.address, previousStatus)
 
-    case Add(key, value) =>
+    case Add(key : String, value : String) =>
+      val write = WriteTo(n = 1, timeout = 5.seconds)
+
+      kkey = key
+      val Counter1Key = PNCounterKey(key)
+      replicator ! Update(dataMap, ORSet.empty[(String, String)],write)(_ + (key.toString, value.toString))
       println("dodawanie " + key + " " + value)
+
+      val read = ReadLocal
+      replicator ! Get(Counter1Key, read)
+
+    case g @ GetSuccess(_, req) =>
+      val value = g.get(PNCounterKey(kkey)).value
+      println("jest tutaj" + value)
+    case NotFound(_, req) => // key counter1 does not exist
+      println("nie ma tutaj")
+
       // TODO
     case GetOne(key) =>
       //TODO
