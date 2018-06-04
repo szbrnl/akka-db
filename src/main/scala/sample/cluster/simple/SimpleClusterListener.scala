3facbe11e21bd9606fd3383042a846f244d217ca
Szymon Bernal <szymon.bernal@gmail.com>
2018-06-04 21:50:50 +0200

    code cleanup

--- src/main/scala/sample/cluster/simple/SimpleClusterListener.scala
+++ src/main/scala/sample/cluster/simple/SimpleClusterListener.scala
@@ -27,28 +27,17 @@ class SimpleClusterListener extends Actor with ActorLogging {
 
   def receive = {
     case msg: String =>
-      log.info("hello darkness my old friend")
+      log.info(msg)
 
     case MemberUp(member) =>
       log.info("Member is Up: {}", member.address)
       if (member.hasRole("frontendapi")) {
         context.actorSelection(RootActorPath(member.address) / "user" / "frontend-api") !
           BackendRegistration
-        println("test receive'a\n")
-
       }
 
-      for (i <- cluster.state.members) {
-        println("asdasdasd")
-        if (!i.hasRole("frontendapi"))
-          context.actorSelection(RootActorPath(member.address) / "user" / "clusterListener") ! "asdasdasd"
-      }
-
-      println("mam gooooooooo")
 
     case UnreachableMember(member) =>
-
-      //     println(dataMap.getEntries())
       log.info("Member detected as unreachable: {}", member)
 
 
@@ -60,12 +49,12 @@ class SimpleClusterListener extends Actor with ActorLogging {
     case Add(key: String, value: String) =>
       println("dodawanie " + key + " " + value)
 
-
       val keyPos = key.hashCode % 1000000
       var min = 1000000
       var minAddr: Address = null
 
 
+      // TODO refactor
       for (member <- cluster.state.members) {
         if (!member.hasRole("frontendapi") && member.address.hashCode % 1000000 >= keyPos && member.address.hashCode % 1000000 < min) {
           min = member.address.hashCode
@@ -78,20 +67,15 @@ class SimpleClusterListener extends Actor with ActorLogging {
         var min = 1000000
 
         for (member <- cluster.state.members) {
-          if (member.address.hashCode % 1000000 < min)
+          if (member.address.hashCode % 1000000 <= min)
             minAddr = member.address
         }
-
       }
-
       context.actorSelection(RootActorPath(minAddr) / "user" / "clusterListener") ! RealAdd(key, value)
 
 
     case RealAdd(key: String, value: String) =>
       println("real add: " + key + " " + value)
-
-
-
 
 
     // TODO
